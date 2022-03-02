29c7401391456fc246ae0942657fce51acc8a35c
FrancoisCouture <CoutureFrancois97@hotmail.com>
2022-03-02 18:08:28 -0400

    setup

---
+++ P1.js
@@ -0,0 +1,33 @@
+let cap = false;
+function addChar(selection) {
+  // Get the value from the id'ed field
+  var currChars = $("#words").val();
+
+  if (selection === "bksp") {
+    // Set the id'ed field to a shortened string
+    $("#words").val(currChars.substring(0, currChars.length - 1));
+  } else {
+    // Set the id'ed field to the longer string
+    if (cap === true){
+        $("#words").val(currChars.concat(selection.toUpperCase()));
+        document.getElementById("Caps").innerHTML = "Caps Off!";
+        cap = false;
+    }
+    else{
+        $("#words").val(currChars.concat(selection));
+    }
+    
+  }
+}
+
+function enter() {
+  var content = $("#words").val();
+  console.log(content);
+  $("#words").val("");
+}
+
+function caps() {
+    document.getElementById("Caps").innerHTML = "Caps On!";
+    cap = true;
+
+}
