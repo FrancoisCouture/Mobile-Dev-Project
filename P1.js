bcc0834255feb3ae5b8d23b098722b48cc882f22
Mark <100807187+mark-emmanuel@users.noreply.github.com>
2022-03-09 21:07:36 -0400

    Update P1.js
    code I had earlier, need to make the toggle work both ways though

--- P1.js
+++ P1.js
@@ -1,9 +1,33 @@
-let cap = false;
+// toggle functionality----------------------------------------------------
+const HIDDEN = "hidden";
+let toggle = false;
+var blog1edit = document.getElementById("editBlog1");
 
 function setup() {
-  document.getElementById("keyboard").style.display = "hidden";
+  document.getElementById("keyboard").style.visibility = "hidden";
 }
 
+function editBlog1() {
+  document.getElementById("editBlog2").style.visibility = "hidden";
+  document.getElementById("editBlog3").style.visibility = "hidden";
+  document.getElementById("keyboard").style.visibility = "visible";
+}
+
+function editBlog2() {
+  document.getElementById("editBlog1").style.visibility = "hidden";
+  document.getElementById("editBlog3").style.visibility = "hidden";
+  document.getElementById("keyboard").style.visibility = "visible";
+}
+
+function editBlog3() {
+  document.getElementById("editBlog1").style.visibility = "hidden";
+  document.getElementById("editBlog2").style.visibility = "hidden";
+  document.getElementById("keyboard").style.visibility = "visible";
+}
+
+// end of toggle functionality-------------------------------------------
+
+let cap = false;
 function addChar(selection) {
   // Get the value from the id'ed field
   var currChars = $("#words").val();
