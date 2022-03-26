cb1bc0f491b633de8236330e6d3331a76694fdb9
mark-emmanuel <100807187+mark-emmanuel@users.noreply.github.com>
2022-03-25 23:50:27 -0300

    word bank display
    functionality still in progress

--- P1.js
+++ P1.js
@@ -10,7 +10,7 @@ function setup() {
 
   $("#eb3").val("off");
 
-  for(var i = 0; i < x.length; i++){
+  for (var i = 0; i < x.length; i++) {
     x[i].innerText = x[i].innerText.toLowerCase();
   }
 
@@ -19,89 +19,89 @@ function setup() {
 
 
 function editBlog1() {
-  if ($("#eb1").val() == "off"){
-	$("#wrapper").hide();
-	$("#keyboard").show();
+  if ($("#eb1").val() == "off") {
+    $("#wrapper").hide();
+    $("#keyboard").show();
     $("#words1").show();
     $("#eb1").val("on");
-    act ="words1";
+    act = "words1";
   }
   else {
     $("#wrapper").show();
-	$("#keyboard").hide();
+    $("#keyboard").hide();
     $("#words1").hide();
     $("#eb1").val("off");
-	  $("#words").val("");
+    $("#words").val("");
   }
 }
 
 function editBlog2() {
-  if ($("#eb2").val() == "off"){
-	$("#wrapper").hide();
-	$("#keyboard").show();
+  if ($("#eb2").val() == "off") {
+    $("#wrapper").hide();
+    $("#keyboard").show();
     $("#words2").show();
     $("#eb2").val("on");
-    act ="words2";
+    act = "words2";
   }
   else {
     $("#wrapper").show();
-	$("#keyboard").hide();
+    $("#keyboard").hide();
     $("#words2").hide();
     $("#eb2").val("off");
-	  $("#words").val("");
+    $("#words").val("");
   }
 }
 
 function editBlog3() {
-  if ($("#eb3").val() == "off"){
-	$("#wrapper").hide();
-	$("#keyboard").show();
+  if ($("#eb3").val() == "off") {
+    $("#wrapper").hide();
+    $("#keyboard").show();
     $("#words3").show();
     $("#eb3").val("on");
-    act ="words3";
+    act = "words3";
   }
   else {
     $("#wrapper").show();
-	$("#keyboard").hide();
+    $("#keyboard").hide();
     $("#words3").hide();
     $("#eb3").val("off");
-	  $("#words").val("");
+    $("#words").val("");
   }
 }
 
-// end of toggle functionality-------------------------------------------
+// end of toggle functionality-----------------------------------------------------------------------------------
 
 let cap = false;
 function addChar(selection) {
   // Get the value from the id field
-  var currChars = $("#"+ act).val();
+  var currChars = $("#" + act).val();
 
   if (selection === "bksp") {
     // Remove one char
-    $("#"+ act).val(currChars.substring(0, currChars.length - 1));
+    $("#" + act).val(currChars.substring(0, currChars.length - 1));
   } else {
     if (cap === true) {
       // Place one upper case char
-      $("#"+ act).val(currChars.concat(selection.toUpperCase()));
+      $("#" + act).val(currChars.concat(selection.toUpperCase()));
       caps();
     } else {
       // Place one lower case char
-      $("#"+ act).val(currChars.concat(selection));
-      if(selection == "."){
+      $("#" + act).val(currChars.concat(selection));
+      if (selection == ".") {
         console.log("end of sentence!");
-        $("#"+ act).val(currChars.concat(". "));
+        $("#" + act).val(currChars.concat(". "));
         cap = false;
         caps();
       }
     }
-    
+
   }
 }
 
 // New line break
 function enter() {
-  var currChars = $("#"+ act).val();
-  $("#"+ act).val(currChars.concat("\n"));
+  var currChars = $("#" + act).val();
+  $("#" + act).val(currChars.concat("\n"));
 }
 
 // Toggle caps lock
@@ -109,52 +109,83 @@ let x = document.getElementsByClassName("ltr");
 
 function caps() {
   console.log(x.length);
-  if(cap == false){ 
+  if (cap == false) {
     console.log("cap is true!");
     cap = true;
-    for(var i = 0; i < x.length; i++){
-    x[i].innerText = x[i].innerText.toUpperCase();
+    for (var i = 0; i < x.length; i++) {
+      x[i].innerText = x[i].innerText.toUpperCase();
     }
   } else {
     cap = false;
-    for(var i = 0; i < x.length; i++){
+    for (var i = 0; i < x.length; i++) {
       x[i].innerText = x[i].innerText.toLowerCase();
     }
   }
 }
 
 
-function saveTitle(){
-  if ($("#eb1").val() == "on"){
+function saveTitle() {
+  if ($("#eb1").val() == "on") {
     $("#wrapper").show();
-	$("#keyboard").hide();
+    $("#keyboard").hide();
     $("#words1").hide();
     $("#eb1").val("off");
-	$("#words").val("");
-	$("#eb1_1").prop( "checked", false );
+    $("#words").val("");
+    $("#eb1_1").prop("checked", false);
   }
-  if ($("#eb2").val() == "on"){
+  if ($("#eb2").val() == "on") {
     $("#wrapper").show();
-	$("#keyboard").hide();
+    $("#keyboard").hide();
     $("#words2").hide();
     $("#eb2").val("off");
-	$("#words").val("");
-	$("#eb2_1").prop( "checked", false );
+    $("#words").val("");
+    $("#eb2_1").prop("checked", false);
   }
-  if ($("#eb3").val() == "on"){
+  if ($("#eb3").val() == "on") {
     $("#wrapper").show();
-	$("#keyboard").hide();
+    $("#keyboard").hide();
     $("#words3").hide();
     $("#eb3").val("off");
-	$("#words").val("");
-	$("#eb3_1").prop( "checked", false );
+    $("#words").val("");
+    $("#eb3_1").prop("checked", false);
   }
 }
 
 // The last text box clicked becomes the "active" textbox. 
 // This means when the keyboard is typed, values will appear in the active textbox.
 let act;
-function active(){
+function active() {
   act = document.activeElement.id;
-  console.log("active element is now: "+ act);
+  console.log("active element is now: " + act);
+}
+
+// JS for word bank -----------------------------------------------------------------------------------------------------
+
+function saveWord() {
+  console.log("attempt to save word");
+  var userInput = document.getElementById("words1").value;
+  var n = userInput.split(" ");
+  console.log(n[n.length - 1]);
+  return n[n.length - 1];
+}
+
+function savePhrase() {
+  console.log("attempt to save phrase");
+  var userInput = document.getElementById("words1").value;
+  var n = userInput.split(". ");
+  console.log(n[n.length - 1]);
+  return n[n.length - 1];
+}
+
+function undo() {
+  console.log("attempt to undo");
+  var userInput = document.getElementById("words1").value;
+  let indexWithoutLastWord = userInput.lastIndexOf(" ");
+  var newText = userInput.substring(0, indexWithoutLastWord);
+  console.log(newText);
+  document.getElementById("words1").value = newText;
+}
+
+function deleteItem() {
+  console.log("attempt to delete item from word bank");
 }
